f1514c6bd33915ba682166525c06a708d5cf49af
Paweł J <jpawz@outlook.com>
2023-01-08 17:39:14 +0100

    complete exercise #04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...items) {
+    let filteredArray = arr;
+    items.forEach((item) => filteredArray = filteredArray.filter((i) => i !== item));
+    return filteredArray;
 };
 
 // Do not edit below this line

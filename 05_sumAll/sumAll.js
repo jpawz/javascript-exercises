a2a417c7feef058302753a69a5f37c3e80b1bb14
Paweł J <jpawz@outlook.com>
2023-01-08 17:55:49 +0100

    complete exercise #05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if((typeof start !== 'number') 
+        || (typeof end !== 'number')
+        || (start < 0)
+        || (end < 0)) {
+        return "ERROR";
+    }
 
+    if(start > end) {
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    let sum = 0;
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

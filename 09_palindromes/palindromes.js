be0df352df3af2e94a1bcd1e60adb21ebe80dd97
Paweł J <jpawz@outlook.com>
2023-01-12 17:01:20 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const fixedStr = str.toLowerCase().replace(/\W/g, '');
+    for (let i = 0; i < fixedStr.length / 2; i++) {
+        if(fixedStr[i] !== fixedStr[fixedStr.length - 1 - i]) {
+            return false;
+        }        
+    }
+    return true;
 };
 
 // Do not edit below this line

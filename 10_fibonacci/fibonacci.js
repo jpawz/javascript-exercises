e16a43780e8f15f98e920d610da2e95acb43bdab
Paweł J <jpawz@outlook.com>
2023-01-12 17:36:21 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (number < 0) {
+    return "OOPS";
+  }
+  let a = 0;
+  let b = 1;
 
+  let c = 0;
+
+  for (let i = 2; i <= number; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return number ? b : a;
 };
 
 // Do not edit below this line
